d84a0fe27332b91e676007c76790eb9656cd13f6
anujsamdariya07 <anujsamdariya07@gmail.com>
2025-12-30 23:59:38 +0530

    axios baseURL to remove development environment condition

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === 'development'
-      ? 'http://localhost:5000/api'
-      : 'https://mini-user-management-system-l8nf.onrender.com/api',
+  baseURL: 'https://mini-user-management-system-l8nf.onrender.com/api',
   withCredentials: true,
 });
